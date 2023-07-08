ae74c10db190efe73ea86b348afbdef155dcee5d
Adwait Kesharwani <adwait1291@gmail.com>
2023-07-08 12:23:16 +0530

    Update CSVToList.py

--- Matching-Scripts/Utils/CSVToList.py
+++ Matching-Scripts/Utils/CSVToList.py
@@ -1,8 +1,6 @@
-
 import pandas as pd
 
 def csvtolist(don, rec):
-
     don["userbg"] = don["UID"] + don["BloodGroup"]
     don['userbg'] = don['userbg'].str.replace('+','p')
     don['userbg'] = don['userbg'].str.replace('-','n')
